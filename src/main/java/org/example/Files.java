e339591a102735cbe571948801c9211778427913
NitinSinghR <nitin.singh@tringapps.com>
2023-02-08 15:53:26 +0530

    Add files vai upload

--- src/main/java/org/example/Files.java
+++ src/main/java/org/example/Files.java
@@ -2,12 +2,14 @@ package org.example;
 
 import java.io.*;
 import java.util.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Files {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("C:\\Users\\Tringapps-user4\\Documents\\Paragraph.txt");
+        String path="C:\\Users\\Tringapps-user4\\Documents\\Paragraph.txt";
+        File file = new File(path);
         Logger l=Logger.getLogger("com.api.jar");
         HashMap<String,Integer> map = new HashMap<>();
 
@@ -27,12 +29,12 @@ public class Files {
             }
         }
         sc.close();
-        l.info("The values in files are: "+ map);
+        l.log(Level.INFO,()->"The values in files are: "+ map);
 
         List<Map.Entry<String, Integer>> sorted = map.entrySet().stream()
                 .sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).toList();
 
-        l.info("After Sorting the values in the file: "+ sorted);
+        l.log(Level.INFO,()->"After Sorting the values in the file: "+ sorted);
     }
 
 }
